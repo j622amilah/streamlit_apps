277fd52db7bfba4d87272520efcb4180362ced63
Jamilah Foucher <91731082+j622amilah@users.noreply.github.com>
2022-11-06 17:41:33 +0100

    Add files via upload

--- my_script.py
+++ my_script.py
@@ -1,7 +1,13 @@
-# Created by Jamilah Foucher, le 18 avril 2022
+# cd Documents/PROGRAMMING/Github_analysis_PROJECTS/
+# streamlit run my_script.py
+# strg + w
 
-# une fonction qui trie un vecteur
 import numpy as np
+import streamlit as st
+
+# from streamlit_option_menu import option_menu
+# from streamlit_shap import st_shap
+# from streamlit_echarts import st_echarts#set settings for streamlit page
 
 def sortarr(arr):
     sarr = []
@@ -29,10 +35,58 @@ def sortarr(arr):
     
     return ind, unq, nonunq
 
+# with st.sidebar:
+#     selected = option_menu(None, ["Log In", "Title here"], 
+#     icons=['house',  "list-task"], 
+#     menu_icon="cast", default_index=0, orientation="vertical")
+    
+    
+def head():
 
-A = [2, 4, 6, 4, 3]
-A = np.array(A)
-print('A : ', A)
+    st.title('Welcome to this test application!!')
+    # OR
+    st.markdown("""
+        <h1 style='text-align: center; margin-bottom: -35px;'>
+        Test Streamlit: Sort an array
+        </h1>
+    """, unsafe_allow_html=True
+    )
+    
+    st.caption("""
+        <p style='text-align: center'>
+        by <a href='https://github.com/j622amilah/streamlit_apps'>Github_repo</a>
+        </p>
+    """, unsafe_allow_html=True
+    )
+    
+    st.write(
+        "This is a silly application to learn how to use streamlit.",
+        "Working with applications on the Internet seems difficult.",
+        "Click the button \U0001F642."
+    )
+    return
 
-out = sortarr(A)
-print('sorted A:', out)
+
+def body(arr):
+    out = sortarr(arr)
+    st.info('sorted array}', icon='\U0001F916')
+    st.write(out)
+    
+
+
+
+head()
+
+if st.button('Start Body code'):
+    # df = read_data('filename.csv')
+    # choice = df.sample(1)
+    A = [2, 4, 6, 4, 3]
+    A = np.array(A)
+    st.write('A : ', A)
+    
+    body(A)
+
+
+
+
+

85e96050e3aee157f056379d076308a64feacce6
Jamilah Foucher <91731082+j622amilah@users.noreply.github.com>
2022-11-12 23:13:00 +0100

    Update my_script.py

--- my_script.py
+++ my_script.py
@@ -2,6 +2,8 @@
 # streamlit run my_script.py
 # strg + w
 
+!pip install tensorflow
+
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 import io
